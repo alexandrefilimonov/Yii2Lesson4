34f607f331dddced8293fc9036484b0e8cb2cdf6
alexandrefilimonov <alexflido@yahoo.com>
2018-09-06 01:33:49 -0700

    YII2 LESSON4 Home Work

---
+++ SQL_scriptToCreateTable.sql
@@ -0,0 +1,32 @@
+﻿USE world;
+CREATE TABLE IF NOT EXISTS `user` 
+(
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+   `author` varchar(30) NOT NULL,
+   `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY (`id`) 	
+) ENGINE=InnoDB  DEFAULT CHARSET=UTF8MB4 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `note` 
+(
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+   `note_info` varchar(30) NOT NULL,
+   `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY (`id`) 	
+) ENGINE=InnoDB  DEFAULT CHARSET=UTF8MB4 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `access` 
+(
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+   `note_id` int(11) NOT NULL,
+   `user_id` int(11) NOT NULL,
+   `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY (`id`), 
+   FOREIGN KEY (`note_id`)
+        REFERENCES note(`id`)
+        ON DELETE CASCADE, 
+   FOREIGN KEY (`user_id`)
+        REFERENCES user(`id`)
+        ON DELETE CASCADE 		
+) ENGINE=InnoDB  DEFAULT CHARSET=UTF8MB4 AUTO_INCREMENT=6 ;
+
---
+++ views_note_index.php
@@ -0,0 +1,63 @@
+﻿<?php
+
+
+/* @var $this yii\web\View */
+
+/* @var $form yii\bootstrap\ActiveForm */
+
+/* @var $model app\models\ContactForm */
+
+
+use yii\helpers\Html;
+
+use yii\bootstrap\ActiveForm;
+
+use yii\captcha\Captcha;
+
+
+//$this->title = 'Contact';
+
+//$this->params['breadcrumbs'][] = $this->title;
+
+?>
+<div class='"note-index">
+	<h1><?= Html::encode($this->title) ?></h1>
+	<p>
+		<?= Html::a('CreateNote', ['create'], ['class' => 'btn btn-success']) ?>
+	</p>
+	<?= GridView::widget({
+		'dataProvider' => $dataProvider, 		
+		'filterModel' => $searchModel, 
+		'columns' => [
+			['class' => \yii\grid\SerialColumn::class], 
+				'name',
+				'author.username', 
+			//[
+			//	'format' => 'raw', 
+			//	'value' => function (Note $model) {
+			//		return '';
+			//	}			
+			//],
+
+			[
+				'attribute' => 'created_at',
+				'format' => ['date', 'php:d.m.Y'],			
+			],
+			[
+				'attribute' => 'updated_at',
+				'format' => ['date', 'php:d.m.Y H:i:s'],
+			],
+
+			[
+				'class' => \yii\grid\ActionColumn::class,
+				'visibleButtons' => [
+					'view' => function (\appmodels\Note $model) {
+						return (new NoteAccessChecker())->isAllowedToRead($model);
+					},
+					'update' => $isAllowedToWriteCallback;
+					'delete' => $isAllowedToWriteCallback;
+				],	
+			],
+		],
+	]); ?>
+</div>
